f306121503e8b30bc9860506409fc9e843b11488
dungxtd <dungxtd20@gmail.com>
2024-07-08 10:27:25 +0700

    git script

---
+++ scripts/git.sh
@@ -0,0 +1,9 @@
+#git undo commit
+git reset HEAD~
+
+#config uname and email
+git config user.name "dungxtd"
+git config user.email dungxtd20@gmail.com
+
+git config --global user.name "Your Name Here"
+git config --global user.email your@email.example
