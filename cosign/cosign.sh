9fd83138f445cf877513dd3cc06ab0dc60670a77
dungxtd <dungxtd20@gmail.com>
2024-09-22 03:22:22 +0700

    docker file

--- cosign/cosign.sh
+++ cosign/cosign.sh
@@ -48,3 +48,51 @@ docker run --rm -it \
       --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
       ddplatform-pvt-registry:443/zubin-ui:16461 | jq .
     "
+
+
+docker run --rm -it \
+  --add-host ddplatform-pvt-registry:10.210.24.205 \
+  -e SM_API_KEY=015b5f56d7812a6599884cbc68_b6c77cc17480f2a71c2b1e09a572e007dff0d53634e201ad566c1a573e30ca6b \
+  -e SM_HOST=https://clientauth.one.digicert.com \
+  -e SM_CLIENT_CERT_FILE=/usr/local/lib/certificate_pkcs12.p12 \
+  -e SM_CLIENT_CERT_PASSWORD=eR9JeE1oeT6H \
+  -v /root/cosign/certificate_pkcs12.p12:/usr/local/lib/certificate_pkcs12.p12 \
+  -v /root/cosign/libp11.so:/usr/local/lib/libp11.so \
+  -v /root/cosign/key_922268116.pem:/usr/local/lib/key_922268116.pem \
+  cosign sh -c "\
+    cosign login ddplatform-pvt-registry:443 \
+      --username=ddpadmin --password=User@123 && \
+    cosign copy ddplatform-pvt-registry:443/zubin-ui:16487 10.210.60.130:5000/zubin-ui:16487 -f && \
+    cosign clean 10.210.60.130:5000/zubin-ui:16487 -f && \
+    cosign sign \
+      --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
+      10.210.60.130:5000/zubin-ui:16487 \
+      -y -a author='Data Dynamics' -a project='Zubin' -a signedDate='$(date +%Y-%m-%dT%H:%M:%S)' && \
+    cosign verify \
+      --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
+      10.210.60.130:5000/zubin-ui:16487 | jq .
+    "
+
+docker run --rm -it \
+  --add-host ddplatform-pvt-registry:10.210.24.205 \
+  -e SM_API_KEY=015b5f56d7812a6599884cbc68_b6c77cc17480f2a71c2b1e09a572e007dff0d53634e201ad566c1a573e30ca6b \
+  -e SM_HOST=https://clientauth.one.digicert.com \
+  -e SM_CLIENT_CERT_FILE=/usr/local/lib/certificate_pkcs12.p12 \
+  -e SM_CLIENT_CERT_PASSWORD=eR9JeE1oeT6H \
+  -v /root/cosign/certificate_pkcs12.p12:/usr/local/lib/certificate_pkcs12.p12 \
+  -v /root/cosign/libp11.so:/usr/local/lib/libp11.so \
+  -v /root/cosign/key_922268116.pem:/usr/local/lib/key_922268116.pem \
+  cosign sh -c "\
+    cosign login ddplatform-pvt-registry:443 \
+      --username=ddpadmin --password=User@123 && \
+    cosign copy ddplatform-pvt-registry:443/zubin-ui:16487 10.210.60.130:5000/zubin-ui:16487 -f && \
+    cosign clean 10.210.60.130:5000/zubin-ui:16487 -f && \
+    cosign sign \
+      --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
+      10.210.60.130:5000/zubin-ui:16487 \
+      -y -a author='Data Dynamics' -a project='Zubin' -a signedDate='$(date +%Y-%m-%dT%H:%M:%S)'
+    "
+
+
+
+  -e COSIGN_PKCS11_IGNORE_CERTIFICATE=1 \
