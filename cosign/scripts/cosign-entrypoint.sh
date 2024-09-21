61e3dc2b0ea721cfcd8724bb4158030909419de7
dungxtd <dungxtd20@gmail.com>
2024-09-22 06:25:23 +0700

    cosign entry point

--- cosign/scripts/cosign-entrypoint.sh
+++ cosign/scripts/cosign-entrypoint.sh
@@ -28,29 +28,33 @@ nerdctl run --rm -it   -v /usr/local/lib:/usr/local/lib -v /home/temp:/home/temp
 
 ddplatform-pvt-registry:443/zubin-ui:16461
 
+PATH_COSIGN_CRES="/cosign/credentials"
+PATH_COSIGN_DATA="/cosign/data"
+PATH_COSIGN_SCRIPTS="/cosign/scripts"
+PATH_COSIGN_LIB="/cosign/lib"
+IMAGE_NAME="ddplatform-pvt-registry:443/zubin-control-panel:16485"
+TARGET_IMAGE_NAME="10.210.60.130:5000/zubin-ui:16461"
+
 docker run --rm -it \
   --add-host ddplatform-pvt-registry:10.210.24.205 \
   -e COSIGN_PKCS11_IGNORE_CERTIFICATE=1 \
   -e SM_API_KEY=015b5f56d7812a6599884cbc68_b6c77cc17480f2a71c2b1e09a572e007dff0d53634e201ad566c1a573e30ca6b \
   -e SM_HOST=https://clientauth.one.digicert.com \
-  -e SM_CLIENT_CERT_FILE=/usr/local/lib/certificate_pkcs12.p12 \
   -e SM_CLIENT_CERT_PASSWORD=eR9JeE1oeT6H \
-  -v /root/cosign/certificate_pkcs12.p12:/usr/local/lib/certificate_pkcs12.p12 \
-  -v /root/cosign/libp11.so:/usr/local/lib/libp11.so \
-  -v /root/cosign/key_922268116.pem:/usr/local/lib/key_922268116.pem \
-  -v /root/cosign/data:/data \
+  -e SM_CLIENT_CERT_FILE=${PATH_COSIGN_CRES}/certificate_pkcs12.p12 \
+  -e PATH_COSIGN_CRES=${PATH_COSIGN_CRES} \
+  -e PATH_COSIGN_DATA=${PATH_COSIGN_DATA} \
+  -e PATH_COSIGN_SCRIPTS=${PATH_COSIGN_SCRIPTS} \
+  -e PATH_COSIGN_LIB=/usr/local/lib \
+  -e IMAGE_NAME=${IMAGE_NAME} \
+  -e TARGET_IMAGE_NAME=${TARGET_IMAGE_NAME} \
+  -v ${HOME}/${PATH_COSIGN_CRES}:/${PATH_COSIGN_CRES} \
+  -v ${HOME}/${PATH_COSIGN_DATA}:/${PATH_COSIGN_DATA} \
+  -v ${HOME}/${PATH_COSIGN_SCRIPTS}:/${PATH_COSIGN_SCRIPTS} \
+  -v ${HOME}/${PATH_COSIGN_LIB}/libp11.so:/usr/local/lib/libp11.so \
   cosign sh -c "\
-    cosign login ddplatform-pvt-registry:443 \
-      --username=ddpadmin --password=User@123 && \
-    cosign clean ddplatform-pvt-registry:443/zubin-ui:16487 -f && \
-    cosign sign \
-      --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
-      ddplatform-pvt-registry:443/zubin-ui:16487 \
-      -y -a author='Data Dynamics' -a project='Zubin' -a signedDate='$(date +%Y-%m-%dT%H:%M:%S)' && \
-    cosign verify \
-      --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
-      ddplatform-pvt-registry:443/zubin-ui:16487 | jq . && \
-    cosign save --dir /data/ddplatform-pvt-registry:443/zubin-ui:16487 ddplatform-pvt-registry:443/zubin-ui:16487
+      cd ${PATH_COSIGN_SCRIPTS} && \
+      sh cosign-sign.sh
     "
 
 docker run --rm -it \
@@ -63,11 +67,11 @@ docker run --rm -it \
   -v /root/cosign/certificate_pkcs12.p12:/usr/local/lib/certificate_pkcs12.p12 \
   -v /root/cosign/libp11.so:/usr/local/lib/libp11.so \
   -v /root/cosign/key_922268116.pem:/usr/local/lib/key_922268116.pem \
-  -v /root/cosign/data:/data \
+  -v /root/cosign/data:/cosign/data \
   cosign sh -c "\
-    cosign clean 10.210.60.130:5000/zubin-ui:16487 -f && \
-    cosign load --dir /data/ddplatform-pvt-registry:443/zubin-ui:16487 10.210.60.130:5000/zubin-ui:16487 && \
+    cosign clean ${TARGET_IMAGE_NAME} -f && \
+    cosign load --dir /cosign/data/${IMAGE_NAME} ${TARGET_IMAGE_NAME} && \
     cosign verify \
       --key 'pkcs11:token=Virtual%20PKCS%2311%20Token;slot-id=0;id=%32%61%63%63%36%64%38%66%2d%39%35%34%63%2d%34%33%64%61%2d%61%38%32%37%2d%32%36%36%64%34%30%37%66%63%66%34%62;object=key_922268116?module-path=/usr/local/lib/libp11.so' \
-      10.210.60.130:5000/zubin-ui:16487 | jq .
+      ${TARGET_IMAGE_NAME} | jq .
     "
---
+++ cosign/scripts/cosign-sign.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cosign login ddplatform-pvt-registry:443 \
+    --username=ddpadmin --password=User@123
+cosign clean ${IMAGE_NAME} -f
+cosign pkcs11-tool list-tokens --module-path ${PATH_COSIGN_LIB}/libp11.so
+cosign pkcs11-tool list-keys-uris --module-path ${PATH_COSIGN_LIB}/libp11.so --slot-id 0 --pin anything | grep "pkcs11:token=" | while read -r line; do
+    # Extract the part after "URI:" and trim the whitespace
+    uri_value=$(echo "$line" | cut -d ':' -f 2- | awk '{$1=$1; print}')    
+    # Extract the key label from the URI
+    label=$(echo "$uri_value" | grep -oP '(?<=object=key_)\d+')
+    
+    echo "Signing with key $label..." 
+    # Attempt to sign and catch errors
+    if output=$(cosign sign \
+            --key ${uri_value}  \
+            ${IMAGE_NAME} \
+            -y -a author='Data Dynamics' -a project='Zubin' -a signedDate=$(date +%Y-%m-%dT%H:%M:%S) 2>&1); then
+        echo "Signing successful!!!"
+        cosign verify \
+            --key ${uri_value} \
+            ${IMAGE_NAME} | jq .
+    else
+        echo "Error occurred during signing with URI:$uri_value.\n"
+    fi
+done
+
+cosign save --dir /cosign/data/${IMAGE_NAME} ${IMAGE_NAME}
