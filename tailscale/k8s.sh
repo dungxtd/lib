6a11b9ff9da2766c678be899b8fbb231d098f8e9
dungxtd <dungxtd20@gmail.com>
2025-03-15 22:25:56 +0700

    Add installation instructions for Lens and Tailscale Helm chart

---
+++ tailscale/k8s.sh
@@ -0,0 +1,14 @@
+helm repo add tailscale https://pkgs.tailscale.com/helmcharts
+
+helm repo update
+
+helm upgrade \
+  --install \
+  tailscale-operator \
+  tailscale/tailscale-operator \
+  --namespace=tailscale \
+  --create-namespace \
+  --set-string oauth.clientId="<OAauth client ID>" \
+  --set-string oauth.clientSecret="<OAuth client secret>" \
+  --set-string apiServerProxyConfig.mode="true" \
+  --wait
