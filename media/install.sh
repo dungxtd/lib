ec4830b6e04cddb895ef925232a00a29d9180b02
dungxtd <dungxtd20@gmail.com>
2025-03-16 03:27:59 +0700

    Add Helm chart installations for Jackett and Flaresolverr with updated configurations

--- media/install.sh
+++ media/install.sh
@@ -1,2 +1,26 @@
-helm repo add k8s-at-home https://k8s-at-home.com/charts/
+helm repo add rubxkube https://rubxkube.github.io/charts/
 helm repo update
+
+helm upgrade --install jackett rubxkube/jackett \
+  --set common.image.tag=latest \
+  --set common.service.type=LoadBalancer \
+  --set common.persistence.volumes[0].name="config" \
+  --set common.persistence.volumes[0].storageClassName="longhorn" \
+  --set common.persistence.volumes[0].size="2Gi" \
+  --set common.persistence.volumes[0].containerMount="/config" \
+  --set common.persistence.volumes[1].name="downloads" \
+  --set common.persistence.volumes[1].storageClassName="longhorn" \
+  --set common.persistence.volumes[1].size="5Gi" \
+  --set common.persistence.volumes[1].containerMount="/downloads" \
+  --set common.variables.nonSecret.TZ="Asia/Ho_Chi_Minh" \
+  --set common.variables.nonSecret.AUTO_UPDATE="true" \
+  --set common.variables.nonSecret.JACKETT_AUTOMATIC_SEARCH_TIMEOUT="120000" \
+  --set common.variables.nonSecret.JACKETT_MANUAL_SEARCH_TIMEOUT="15000" \
+  -n media --create-namespace
+
+
+helm upgrade --install flaresolverr rubxkube/flaresolverr \
+  --set common.image.tag=latest \
+  --set common.variables.nonSecret.TZ="Asia/Ho_Chi_Minh" \
+  --set common.variables.nonSecret.FS_BROWSER_TIMEOUT="30000" \
+  -n media --create-namespace
